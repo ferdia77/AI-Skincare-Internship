df0fb6aab7ab2c276ae46061716e0c1cc59d3029
David <kingqueensuited22@gmai.com>
2025-04-24 15:51:14 +0200

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <div className="flex-row">
+        <div className="flex-row">
+            <a className='flex__row--button'>
+                SKINSTRIC
+            </a>
+            <a className='flex__row--button'>
+                [INTRO]
+            </a>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
